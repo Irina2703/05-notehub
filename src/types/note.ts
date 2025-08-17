9e27337ae62a9465920afbdbb6d126ef7f95fd81
Kliucherova Irina <klucerovaira@gmail.com>
2025-08-17 15:42:03 +0200

    commit

--- src/types/note.ts
+++ src/types/note.ts
@@ -2,7 +2,7 @@ export interface Note {
     id: string;
     title: string;
     content: string;
-    createdAt: string;
-    updatedAt: string;
-    tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+    tag?: string;
 }
+
+export type NoteTag = 'personal' | 'work' | 'other';
